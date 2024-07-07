893421a30502e2d4996801b79af0d4e9147de617
Andres Galindo <sharlly1329g@gmail.com>
2024-07-07 00:22:09 -0500

    archivo dry agregado

---
+++ src/clean-code/06-dry.ts
@@ -0,0 +1,23 @@
+
+type Size = ''|'M'|'S'|'XL'|'L';
+
+class Product{
+    constructor (
+        public name: string = '',
+        public price : number = 0,
+        public size : Size = ''
+    ){}
+
+    toString(){
+        // No Dry
+        if (this.name.length <= 0) throw Error("name is empty");
+        if (this.price <= 0) throw Error("price is zero");
+        if (this.size === '') throw Error("size is empty");
+        return `${this.name}, ${this.price}, ${this.size}`;
+    }
+}
+
+(()=>{
+    const bluePants = new Product('Blue large pants', 10, 'L');
+    console.log(bluePants.toString());
+})();
