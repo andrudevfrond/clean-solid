c6253433ce87349a9fb57b798d7911099d783c5b
Andres Galindo <sharlly1329g@gmail.com>
2024-07-08 20:46:42 -0500

    refactorizar el codigo evitando redundancia de código

--- src/clean-code/06-dry.ts
+++ src/clean-code/06-dry.ts
@@ -9,16 +9,31 @@ class Product{
         public size : Size = ''
     ){}
 
+    isProductReady() : boolean {
+        for(const key in this){
+            switch(typeof this[key]){
+                case 'string':
+                    if ((<string><unknown>this[key]).length <= 0) throw Error(`${key} is empty`);
+                break;
+                case 'number':
+                    if ((<number><unknown>this[key]) <= 0) throw Error(`${key} is zero`);
+                break;
+                default:
+                    throw Error(`${typeof this[key]} is not supported`);
+            }
+        }
+        return true;
+    }
+
     toString(){
-        // No Dry
-        if (this.name.length <= 0) throw Error("name is empty");
-        if (this.price <= 0) throw Error("price is zero");
-        if (this.size === '') throw Error("size is empty");
+        
+        if (this.isProductReady()) return;
+
         return `${this.name}, ${this.price}, ${this.size}`;
     }
 }
 
 (()=>{
-    const bluePants = new Product('Blue large pants', 10,false,'L');
+    const bluePants = new Product('blue pants', 6);
     console.log(bluePants.toString());
 })();
